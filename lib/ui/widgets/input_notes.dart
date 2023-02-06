2a3f50ae8d46f9ac08f390cfe5cfcbe7969c6b61
Priyanshu-Singhz <ps4151722@gmail.com>
2023-02-06 22:24:36 +0530

    Implemented controller verywhere

--- lib/ui/widgets/input_notes.dart
+++ lib/ui/widgets/input_notes.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../theme.dart';
-  
+    
 class NoteField extends StatelessWidget {
   final String hint;
   final TextEditingController? controller;
